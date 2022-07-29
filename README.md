ee0a8dc36302b52ae204db9f890f8168189c3434
Jerboa <84378622+Jerboa-app@users.noreply.github.com>
2022-07-29 13:45:52 +0100

    Adds giffing (#5)
    * makes a gif from events
    * updates examples

--- README.md
+++ README.md
@@ -2,7 +2,11 @@
 
 Triangulate polygons in Julia using the O(n^2), for n = vertex count, algorithm of ElGindy et al, [Slicing an Ear Using Prune and Search](https://www.sciencedirect.com/science/article/abs/pii/016786559390141Y)
 
-You probably want to use [Triangle.jl](https://github.com/cvdlab/Triangle.jl), [Triangulate.jl](https://github.com/JuliaGeometry/Triangulate.jl), [Meshes.jl](https://github.com/JuliaGeometry/Meshes.jl), [Meshing.jl](https://github.com/JuliaGeometry/Meshing.jl), or [VoronoiDelaunay.jl](https://github.com/JuliaGeometry/VoronoiDelaunay.jl)
+The code is built purposely to rely only on base Julia, rather than use the Julia geometry ecosystem, to give an example of a fully standalone implementation of the algorithm. Julia's geometry ecosystem provides a few more efficient algorithms!
+
+![examples](https://raw.githubusercontent.com/Jerboa-app/PolygonTriangulation.jl/adds_giffing/examples/steps.gif)
+
+Some bugs persist on large polygons (~N=32), where triangles form outside the perimiter, these are reasonably rare. So, you probably want to use [Triangle.jl](https://github.com/cvdlab/Triangle.jl), [Triangulate.jl](https://github.com/JuliaGeometry/Triangulate.jl), [Meshes.jl](https://github.com/JuliaGeometry/Meshes.jl), [Meshing.jl](https://github.com/JuliaGeometry/Meshing.jl), or [VoronoiDelaunay.jl](https://github.com/JuliaGeometry/VoronoiDelaunay.jl)
 
 However Triangulate.jl and Triangle.jl rely on the [Triangle library](http://www.cs.cmu.edu/~quake/triangle.html) which can't be used commerically without a license direct from the author.
 
--- src/EventPlot.jl
+++ src/EventPlot.jl
@@ -2,25 +2,90 @@ function eventFrames(p::Polygon,events::Vector{PolygonTriangulation.Event})
     fig = Figure()
     ax = Axis(fig[1,1])
 
-    record(fig,"steps.gif",framerate=1,0:length(events)-1) do i
+    q = Polygon(P.vertices)
+    gsp = nothing
+    triangles = []
+    alpha = 1.0
 
-        if (i == 0)
-            baseFrame!(ax,P)
-        else
-            eventFrame!(ax,p,events[i])
+    record(fig,"steps.gif",framerate=1,1:length(events)-1) do i
+
+        e = events[i]
+        empty!(ax)
+
+        if (typeof(e)==PolygonTriangulation.GoodSubPolygonEvent)
+            gsp = e
+        end
+
+        baseFrame!(ax,P,alpha)
+
+        if (alpha < 1.)
+            if (gsp != nothing)
+                eventFrame!(ax,q,gsp)
+            else
+                baseFrame!(ax,q,1.)
+            end
+        elseif (gsp != nothing)
+            eventFrame!(ax,q,gsp)
+        end
+
+
+        if (typeof(e)!=PolygonTriangulation.GoodSubPolygonEvent)
+            eventFrame!(ax,q,e)
+        end
+
+        if (typeof(e)==PolygonTriangulation.FoundEarEvent)
+            alpha = 0.33
+            v = []
+            for i in 1:length(q)
+                if (q.vertices[i] != e.ear)
+                    push!(v,q.vertices[i])
+                end
+            end
+            q = Polygon(v)
+            gsp = nothing
+        end
+
+        if (typeof(e) == PolygonTriangulation.FoundTriangleEvent)
+            push!(triangles,e)
+        end
+
+        for evtTri in triangles
+            eventFrame!(ax,P,evtTri)
         end
     end
+end
+
+function eventFrame!(ax::Axis,p::Polygon,evt::PolygonTriangulation.FoundEarEvent)
+    scatter!(ax,[Point2f0(v.x,v.y) for v in p.vertices],color=RGBAf0(0.,0.,1.,1.0))
+    e = Vector{Point2f0}([])
+    for edge in PolygonTriangulation.edges(p)
+        push!(e,Point2f0(edge[1].x,edge[1].y))
+        push!(e,Point2f0(edge[2].x,edge[2].y))
+    end
+    scatter!(ax,[Point2f0(evt.ear.x,evt.ear.y)],color=:white,marker=:cross)
+    linesegments!(ax,e,color=RGBAf0(0.,0.,1.,1.0))
+end
+
+function eventFrame!(ax::Axis,p::Polygon,evt::PolygonTriangulation.GoodSubPolygonEvent)
+    q = evt.p
+    scatter!(ax,[Point2f0(v.x,v.y) for v in q.vertices],color=RGBAf0(0.,0.,1.,1.0))
+    e = Vector{Point2f0}([])
+    for edge in PolygonTriangulation.edges(q)
+        push!(e,Point2f0(edge[1].x,edge[1].y))
+        push!(e,Point2f0(edge[2].x,edge[2].y))
+    end
+    linesegments!(ax,e,color=RGBAf0(0.,1.,1.,1.0),linestyle=:dot)
 
 end
 
-function baseFrame!(ax::Axis,P::Polygon)
-    scatter!(ax,[Point2f0(v.x,v.y) for v in P.vertices])
+function baseFrame!(ax::Axis,P::Polygon,alpha=1.0)
+    scatter!(ax,[Point2f0(v.x,v.y) for v in P.vertices],color=RGBAf0(0.,0.,1.,alpha))
     e = Vector{Point2f0}([])
     for edge in PolygonTriangulation.edges(P)
         push!(e,Point2f0(edge[1].x,edge[1].y))
         push!(e,Point2f0(edge[2].x,edge[2].y))
     end
-    linesegments!(ax,e)
+    linesegments!(ax,e,color=RGBAf0(0.,0.,1.,alpha))
 end
 
 function eventFrame!(ax::Axis,p::Polygon,evt::PolygonTriangulation.TestingVertexEvent)
@@ -28,7 +93,7 @@ function eventFrame!(ax::Axis,p::Polygon,evt::PolygonTriangulation.TestingVertex
 end
 
 function eventFrame!(ax::Axis,p::Polygon,evt::PolygonTriangulation.BisectorEvent)
-    lines!(ax,[Point2f0(evt.o.x,evt.o.y),Point2f0(evt.ray.x,evt.ray.y)],color=:red,linestyle=:dash)
+    lines!(ax,[Point2f0(evt.o.x,evt.o.y),Point2f0(evt.o.x+evt.ray.x,evt.o.y+evt.ray.y)],color=:red,linestyle=:dash)
 end
 
 function eventFrame!(ax::Axis,p::Polygon,evt::PolygonTriangulation.IntersectionEvent)
@@ -50,41 +115,6 @@ function eventFrame!(ax::Axis,p::Polygon,evt::PolygonTriangulation.FoundDiagonal
     lines!(ax,[Point2f0(evt.a.x,evt.a.y),Point2f0(evt.b.x,evt.b.y)],color=:black)
 end
 
-function eventFrame!(ax::Axis,p::Polygon,evt::PolygonTriangulation.GoodSubPolygonEvent)
-    empty!(ax)
-
-    scatter!(ax,[Point2f0(v.x,v.y) for v in p.vertices],color=RGBAf0(0.,0.,1.,0.33))
-    e = Vector{Point2f0}([])
-    for edge in PolygonTriangulation.edges(p)
-        push!(e,Point2f0(edge[1].x,edge[1].y))
-        push!(e,Point2f0(edge[2].x,edge[2].y))
-    end
-
-    q = evt.p
-
-    linesegments!(ax,e,color=RGBAf0(0.,0.,1.,0.33))
-    scatter!(ax,[Point2f0(v.x,v.y) for v in q.vertices],color=RGBAf0(0.,0.,1.,1.0))
-    e = Vector{Point2f0}([])
-    for edge in PolygonTriangulation.edges(q)
-        push!(e,Point2f0(edge[1].x,edge[1].y))
-        push!(e,Point2f0(edge[2].x,edge[2].y))
-    end
-    linesegments!(ax,e,color=RGBAf0(0.,0.,1.,1.0))
-
-end
-
-function eventFrame!(ax::Axis,p::Polygon,evt::PolygonTriangulation.FoundEarEvent)
-    empty!(ax)
-    scatter!(ax,[Point2f0(v.x,v.y) for v in p.vertices],color=RGBAf0(0.,0.,1.,1.0))
-    e = Vector{Point2f0}([])
-    for edge in PolygonTriangulation.edges(p)
-        push!(e,Point2f0(edge[1].x,edge[1].y))
-        push!(e,Point2f0(edge[2].x,edge[2].y))
-    end
-    scatter!(ax,[Point2f0(evt.ear.x,evt.ear.y)],color=:white,marker=:cross)
-    linesegments!(ax,e,color=RGBAf0(0.,0.,1.,1.0))
-end
-
 function eventFrame!(ax::Axis,p::Polygon,evt::PolygonTriangulation.FoundTriangleEvent)
     linesegments!(ax,[
         Point2f0(evt.a.x,evt.a.y),
@@ -93,28 +123,5 @@ function eventFrame!(ax::Axis,p::Polygon,evt::PolygonTriangulation.FoundTriangle
         Point2f0(evt.c.x,evt.c.y),
         Point2f0(evt.c.x,evt.c.y),
         Point2f0(evt.a.x,evt.a.y)
-        ],color=:red,linestyle=:dash,linewidth=4)
-end
-
-function eventFrame!(ax::Axis,p::Polygon,evt::PolygonTriangulation.FoundEarEvent)
-    empty!(ax)
-    scatter!(ax,[Point2f0(v.x,v.y) for v in p.vertices],color=RGBAf0(0.,0.,1.,1.0))
-    e = Vector{Point2f0}([])
-    for edge in PolygonTriangulation.edges(p)
-        push!(e,Point2f0(edge[1].x,edge[1].y))
-        push!(e,Point2f0(edge[2].x,edge[2].y))
-    end
-    scatter!(ax,[Point2f0(evt.ear.x,evt.ear.y)],color=:white,marker=:cross)
-    linesegments!(ax,e,color=RGBAf0(0.,0.,1.,1.0))
-end
-
-function eventFrame!(ax::Axis,p::Polygon,evt::PolygonTriangulation.FoundTriangleEvent)
-    linesegments!(ax,[
-        Point2f0(evt.a.x,evt.a.y),
-        Point2f0(evt.b.x,evt.b.y),
-        Point2f0(evt.b.x,evt.b.y),
-        Point2f0(evt.c.x,evt.c.y),
-        Point2f0(evt.c.x,evt.c.y),
-        Point2f0(evt.a.x,evt.a.y)
-        ],color=:red,linestyle=:dash,linewidth=4)
+        ],color=RGBAf0(0.5,0.5,0.5,1.0),linewidth=2)
 end
